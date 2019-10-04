0ae0886a15128b13bcb1ae170bad898528163d0c
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 17:43:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_value = nil
-  min_key = nil 
+  min_key = nil
   name_hash.each do |key, value|
-    if value
+    if value < min_value
+      min_key = key
+      min_value = value
+    end
+    min_key
   end
 end
